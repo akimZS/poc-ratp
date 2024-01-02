ed228f5d694f4498666aa2708e3a7a7335a06a0d
Akim <akim.z.santos@basiclabs.io>
2024-01-02 05:04:49 +0100

    feat: add route handler for bus station timetable

--- api/app.ts
+++ api/app.ts
@@ -10,7 +10,6 @@ const PORT = process.env.API_PORT || 3000;
 const app = express();
 
 // view engine setup
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -20,14 +19,41 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Root page.');
 });
 
-app.get('/testratp', async (req: Request, res: Response) => {
+app.get('/bus-timetable/:busStationNumber', async (req: Request, res: Response) => {
+  const busStationNumber = req.params.busStationNumber;
   const result = await axios.get(
-    'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF:StopPoint:Q:463158:',
+    `https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF:StopPoint:Q:${busStationNumber}:`,
     {
       headers: { Accept: 'application/json', apikey: process.env.RATP_DYNAMIC_TOKEN }
     }
   );
-  res.send(JSON.stringify(result.data, null, 2));
+
+  const data = result.data
+  const stationData = data.Siri.ServiceDelivery.StopMonitoringDelivery[0].MonitoredStopVisit;
+  
+  // Print station data
+  const stationName = JSON.stringify(stationData[0].MonitoredVehicleJourney.MonitoredCall.StopPointName[0].value);
+  console.log(`
+    Nom de station: ${stationName}
+    Heure actuelle : ${new Date().toLocaleTimeString('en-GB', {hour: '2-digit', minute: '2-digit', hour12: false})}
+  `);
+  
+  for (let i = 0; i < stationData.length; i++) {
+    const mvj = stationData[i].MonitoredVehicleJourney
+    const line = mvj.OperatorRef.value
+    const direction = mvj.DestinationName[0].value;
+    const timeNext = new Date(mvj.MonitoredCall.ExpectedDepartureTime);
+    const timeNow = new Date();
+    const timeNextCountdown = (Math.abs(timeNext.getTime() - timeNow.getTime()) / (1000 * 60)).toFixed(0);
+    const minuteLabel = parseFloat(timeNextCountdown) > 1 ? "minutes" : "minute";
+
+    console.log((`
+      Ligne: ${line}
+      Direction: ${direction}
+      Prochain passage: ${timeNext.toLocaleTimeString('en-GB', {hour: '2-digit', minute: '2-digit', hour12: false})} (dans ${timeNextCountdown} ${minuteLabel})
+    `))  
+  }
+  res.send(JSON.stringify(data, null, 2))
 });
 
 // catch 404 and forward to error handler
