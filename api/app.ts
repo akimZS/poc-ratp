58b95140ee4f288d35f7a72404e3982932edaaa8
Akim <akim.z.santos@basiclabs.io>
2024-01-02 01:51:01 +0100

    add first test calls + missing dotenv support

--- api/app.ts
+++ api/app.ts
@@ -1,27 +1,39 @@
-import createError from 'http-errors'
-import express, { Request, Response } from 'express'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
+import createError from 'http-errors';
+import express, { Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import axios from 'axios';
+import 'dotenv/config';
 
-const PORT = process.env.API_PORT || 3000
+const PORT = process.env.API_PORT || 3000;
 
-const app = express()
+const app = express();
 
 // view engine setup
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Root page.')
-})
+  res.send('Root page.');
+});
+
+app.get('/testratp', async (req: Request, res: Response) => {
+  const result = await axios.get(
+    'https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring?MonitoringRef=STIF:StopPoint:Q:463158:',
+    {
+      headers: { Accept: 'application/json', apikey: process.env.RATP_DYNAMIC_TOKEN }
+    }
+  );
+  res.send(JSON.stringify(result.data, null, 2));
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(
@@ -29,24 +41,23 @@ app.use(
     err: { message: any; status: any },
     req: { app: { get: (arg0: string) => string } },
     res: {
-      locals: { message: any; error: any }
-      status: (arg0: any) => void
-      render: (arg0: string) => void
-    },
+      locals: { message: any; error: any };
+      status: (arg0: any) => void;
+      render: (arg0: string) => void;
+    }
   ) => {
     // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500)
-    res.render('error')
+    res.status(err.status || 500);
+    res.render('error');
   }
-)
-
+);
 
 app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
 
-module.exports = app
+module.exports = app;
