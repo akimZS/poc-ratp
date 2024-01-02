464bee63802057e43268a5ff4d15592b093290c6
akimZS <89921550+akimZS@users.noreply.github.com>
2024-01-02 17:23:47 +0000

    feat: Add ratp calls (#4)

--- api/app.ts
+++ api/app.ts
@@ -1,27 +1,41 @@
-import createError from 'http-errors'
-import express, { Request, Response } from 'express'
-import cookieParser from 'cookie-parser'
-import logger from 'morgan'
+import createError from 'http-errors';
+import express, { NextFunction, Request, Response } from 'express';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+import 'dotenv/config';
+import { getRoutes } from './routes';
+import { Platforms } from './parsing/platforms';
+import { dataLoader } from './misc';
 
-const PORT = process.env.API_PORT || 3000
+declare global {
+  namespace Express {
+    interface Request {
+      stationsCatalogue: Platforms;
+    }
+  }
+}
 
-const app = express()
+const PORT = process.env.API_PORT || 3000;
+
+const app = express();
+const stationsCatalogue = async (req: Request, res: Response, next: NextFunction) => {
+  req.stationsCatalogue = await dataLoader();
+  next();
+};
+
+app.use(stationsCatalogue);
 
 // view engine setup
-
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-
-app.get('/', (req: Request, res: Response) => {
-  res.send('Root page.')
-})
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(getRoutes);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-  next(createError(404))
-})
+  next(createError(404));
+});
 
 // error handler
 app.use(
@@ -29,24 +43,24 @@ app.use(
     err: { message: any; status: any },
     req: { app: { get: (arg0: string) => string } },
     res: {
-      locals: { message: any; error: any }
-      status: (arg0: any) => void
-      render: (arg0: string) => void
-    },
+      locals: { message: any; error: any };
+      status: (arg0: any) => void;
+      render: (arg0: string) => void;
+    }
   ) => {
     // set locals, only providing error in development
-    res.locals.message = err.message
-    res.locals.error = req.app.get('env') === 'development' ? err : {}
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
     // render the error page
-    res.status(err.status || 500)
-    res.render('error')
+    res.status(err.status || 500);
+    res.render('error');
   }
-)
-
+);
 
 app.listen(PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${PORT}`)
-})
+  console.log(`[server]: Server is running at http://localhost:${PORT}`);
+});
 
-module.exports = app
+export default app;
+export { stationsCatalogue };
